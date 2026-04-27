e57333e69737fd1736d2e355e513eb7e0a8d119a
Jason Neira <jason.neira@us.af.mil>
2026-04-27 11:14:22 -0500

    added add new note button

--- jays-journaling/src/app/page.tsx
+++ jays-journaling/src/app/page.tsx
@@ -1,10 +1,15 @@
 import Image from "next/image";
+import NoteBox from "../components/NoteBox";
+import AddNoteButton from "../components/AddNoteButton";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-600 font-sans">
-      <div className="h-30 font-sans">Jays Journaling App</div>
-      <div className="w-50 h-50 bg-red-500"></div>
+      <div className="flex h-30 font-sans items-center text-5xl">Jay's Journaling App</div>
+      <div className="flex w-450 h-300 bg-red-500 justify-center rounded-lg">
+        <NoteBox></NoteBox>
+        <AddNoteButton></AddNoteButton>
+      </div>
     </div>
   );
 }
