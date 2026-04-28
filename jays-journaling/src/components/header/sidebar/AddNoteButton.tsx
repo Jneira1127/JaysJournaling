04bc4fcd4fb25ae71ae0367a978cb50bee26f800
Jason Neira <jason.neira@us.af.mil>
2026-04-28 13:49:53 -0500

    fixed the placement of the header components

--- jays-journaling/src/components/header/sidebar/AddNoteButton.tsx
+++ jays-journaling/src/components/header/sidebar/AddNoteButton.tsx
@@ -2,7 +2,7 @@ const AddNoteButton = ({ onClick }: { onClick: () => void }) => {
   return (
     <button
       onClick={onClick}
-      className="flex items-center justify-center w-39 h-39 bg-white rounded-lg text-gray-400 text-9xl cursor-pointer"
+      className="flex items-center justify-center w-25 h-25 bg-white rounded-lg text-gray-400 text-9xl cursor-pointer"
     >
       +
     </button>
