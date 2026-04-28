1e141fbfcb7140aa6812f606bfe6e8a1148d81b9
Jason Neira <jason.neira@us.af.mil>
2026-04-28 14:17:34 -0500

    added font styling

--- jays-journaling/src/components/notes/NoteBox.tsx
+++ jays-journaling/src/components/notes/NoteBox.tsx
@@ -7,12 +7,12 @@ const NoteBox = ({ note }: { note: page }) => {
       href={`/note/${note.id}`}
       className="w-67 h-67 bg-white rounded-lg mt-4 ml-4 cursor-pointer"
     >
-      <div className="flex justify-center items-center border-3 border-black h-5 rounded-lg h-50 text-black">
+      <div className="flex justify-center items-center h-5 rounded-lg h-50 text-black">
         <p className="text-sm line-clamp-8 p-2">
           {note.text || "Empty page..."}
         </p>
       </div>
-      <div className="flex justify-center items-center border-black h-5 rounded-sm h-17 text-black bg-gray">
+      <div className="flex justify-center items-center border-black h-20 rounded-sm h-17 text-black bg-gray-500">
         {note.label}
       </div>
     </Link>
